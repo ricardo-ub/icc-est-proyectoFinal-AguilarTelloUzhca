40d310c348e508d1a520bb86745647db169974d4
Ricardo Uzhca <ricardouzhcab27@gmail.com>
2026-07-27 18:27:30 -0500

    UPD: Creacion de archivo y carpetas, MapPoint, Graph, Repositiry

---
+++ src/persistence/FileGraphRepository.java
@@ -0,0 +1,20 @@
+package persistence;
+
+import java.io.IOException;
+
+import models.MapPoint;
+import structures.graphs.Graph;
+
+public class FileGraphRepository implements GraphRepository {
+
+    @Override
+    public void guardar(Graph<MapPoint> grafo, String rutaArchivo) throws IOException {
+        
+    }
+
+    @Override
+    public Graph<MapPoint> cargar(String rutaArchivo) throws IOException {
+        return null;
+    }
+
+}
